899b9403c2f7695960472c8a1d74746126102fd7
Laura_Tosselli <laura.tosselli@gmail.com>
2022-07-26 21:49:52 -0300

    Add files via upload
    Ejercicios de practica de numpy realizados

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -21,6 +21,8 @@ if __name__ == '__main__':
     # potencia_2 = lambda x:......
     # pot_3 = potencia_2(3)
 
+    potencia_2=lambda x: x**2
+
     # 2)
     # Utilice la función map para mapear una lambda expression
     # que retorne la potencia de 2 de cada numero en la lista numeros
@@ -34,7 +36,8 @@ if __name__ == '__main__':
 
     # Lista de numeros
     numeros = [1, -5, 4, 3]
-
+    
     # numeros_potencia = list(map....)
-
+    numeros_pot_2 = list(map(lambda x: x**2, numeros))
+    print('La potencia de los siguientes numeros {} es {}'.format(numeros, numeros_pot_2))
     print("terminamos")
--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -26,9 +26,16 @@ if __name__ == '__main__':
     # para aplicar en este caso.
     list_numeros_str = ['5', '2', '3', '', '7', 'NaN']
 
+    list_int = [int(x) if(x.isdigit()) else 0 for x in list_numeros_str ]
+    print (list_int)
 
     # ¿Ya terminaron el ejercicio? ¿Por qué no prueban
     # hacer negativo alguno de los números de la lista?
     # ¿Qué sucede con isdigit? Sorprendente no?
+    list_numeros_str = ['-5', '2', '3', '', '-7', 'NaN']
+    list_int = [int(x) if(x.isdigit()) else 0 for x in list_numeros_str ]
+    print (list_int)
+
+    '''Al tener algun numero negativo, la función isDigit lo toma como un string'''
 
     print("terminamos")
